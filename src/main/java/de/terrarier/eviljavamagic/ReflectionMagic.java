497eeff7fe40b99f54bdcb7ac77a5892c1fd508d
terrarier2111 <58695553+terrarier2111@users.noreply.github.com>
2021-02-27 18:37:50 +0100

    Reformat

--- src/main/java/de/terrarier/eviljavamagic/ReflectionMagic.java
+++ src/main/java/de/terrarier/eviljavamagic/ReflectionMagic.java
@@ -8,37 +8,37 @@ import sun.misc.Unsafe;
 
 public final class ReflectionMagic {
 
-public static Class<?> getGenericParam(Class<?> clazz, int index) {
-		final ParameterizedType pt = (ParameterizedType) clazz.getGenericSuperclass();
-		// You may need this split or not, use logging to check
-		final String parameterClassName = pt.getActualTypeArguments()[index].toString().split("\\s")[1];
-		try {
-			return Class.forName(parameterClassName);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-  
-  public static Unsafe getUnsafe() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		final Field access = Unsafe.class.getDeclaredField("theUnsafe");
-		access.setAccessible(true);
-	  return (Unsafe) access.get(null);
-	}
-  
-  public static void forceOpenField(Field field) {
-		if(!field.isAccessible()) {
-			field.setAccessible(true);
-		}
-		if((field.getModifiers() & Modifier.FINAL) != 0) {
-			try {
-				final Field modifiers = Field.class.getDeclaredField("modifiers");
-				modifiers.setAccessible(true);
-				modifiers.set(field, field.getModifiers() & ~Modifier.FINAL);
-			} catch (NoSuchFieldException | IllegalAccessException ignored) {
-				
-			}
-		}
-	}
+    public static Class<?> getGenericParam(Class<?> clazz, int index) {
+        final ParameterizedType pt = (ParameterizedType) clazz.getGenericSuperclass();
+        // You may need this split or not, use logging to check
+        final String parameterClassName = pt.getActualTypeArguments()[index].toString().split("\\s")[1];
+        try {
+            return Class.forName(parameterClassName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Unsafe getUnsafe() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        final Field access = Unsafe.class.getDeclaredField("theUnsafe");
+        access.setAccessible(true);
+        return (Unsafe) access.get(null);
+    }
+
+    public static void forceOpenField(Field field) {
+        if (!field.isAccessible()) {
+            field.setAccessible(true);
+        }
+        if ((field.getModifiers() & Modifier.FINAL) != 0) {
+            try {
+                final Field modifiers = Field.class.getDeclaredField("modifiers");
+                modifiers.setAccessible(true);
+                modifiers.set(field, field.getModifiers() & ~Modifier.FINAL);
+            } catch (NoSuchFieldException | IllegalAccessException ignored) {
+
+            }
+        }
+    }
 
 }
